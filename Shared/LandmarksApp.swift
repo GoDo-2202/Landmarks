b041aa4cb316c588d4498d4d16bbb0014d69685a
Godo-2202 <dinhgodo2202>
2022-05-17 21:14:54 +0700

    Initial Commit

---
+++ Shared/LandmarksApp.swift
@@ -0,0 +1,17 @@
+//
+//  LandmarksApp.swift
+//  Shared
+//
+//  Created by Định Nguyễn on 17/05/2022.
+//
+
+import SwiftUI
+
+@main
+struct LandmarksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
